759d703929654079b3caca8ced5b678a219339c3
Simon Mihevc <simon.mihevc@gmail.com>
2019-05-18 23:52:27 +0200

    Added simple stdout mapper

--- streamer/src/streamer/core.clj
+++ streamer/src/streamer/core.clj
@@ -1,7 +1,36 @@
 (ns streamer.core
+  (:import
+   (org.apache.kafka.common.serialization Serde Serdes Serializer)
+   (org.apache.kafka.streams StreamsConfig KafkaStreams StreamsBuilder)
+   (org.apache.kafka.streams.kstream ValueMapper)
+   (org.apache.kafka.streams.kstream Printed)
+)
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn derivative [input-topic]
+  (let [builder (StreamsBuilder.)]
+    (->
+     (.stream builder input-topic) ;; Create the source node of the stream
+	 (.print (Printed/toSysOut )))
+    builder))
+
+
+(defn -main [& args]
+  (def properties
+    {StreamsConfig/APPLICATION_ID_CONFIG, "streamer"
+     StreamsConfig/BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"
+     StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG, (.getName (.getClass (Serdes/String)))
+     StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG, (.getName (.getClass (Serdes/String)))})
+
+  (def config
+    (StreamsConfig. properties))
+
+  (def input-topic "kafka-demo")
+
+  (def streams
+    (KafkaStreams. 
+      (.build (derivative input-topic)) config))
+
+  (.start streams)
+  (Thread/sleep (* 60000 10))
+)

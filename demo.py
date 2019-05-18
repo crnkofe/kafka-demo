47ec97b0170eccee3ee84533648a44c5f559fc49
Simon Mihevc <simon.mihevc@gmail.com>
2019-05-18 21:11:16 +0200

    Fixed kafka config issues. Write not works.

--- demo.py
+++ demo.py
@@ -3,6 +3,8 @@ import time
 import json
 import math
 from kafka import KafkaProducer
+import logging
+logging.basicConfig(level=logging.ERROR)
 
 
 hosts = ['localhost:9092']
@@ -10,20 +12,20 @@ hosts = ['localhost:9092']
 def kafka_serializer(data):
 	return json.dumps(data).encode('utf-8')
 
-producer = KafkaProducer(bootstrap_servers=hosts, value_serializer=kafka_serializer)
+producer = KafkaProducer(bootstrap_servers=hosts, value_serializer=kafka_serializer, request_timeout_ms=1500, max_block_ms=1500)
 
 topic = 'kafka-demo'
 ident = "epic-iot-1"
 
-while !end:
-	try:
-		timestamp_ms = int(round(time.time() * 1000))
-		payload = {
-			"id": ident,
-			"value": math.sin(timestamp_ms)
-		}
-		print(json.dumps(payload))
-		producer.send(topic, value=payload, timestamp_ms=timestamp_ms)
+while True:
+    try:
+        timestamp_ms = int(round(time.time() * 1000))
+        payload = {
+            "id": ident,
+            "value": 1000 * math.sin(((timestamp_ms / 1000) % 360) / 360.0 * 2 * math.pi)
+            }
+        print(json.dumps(payload))
+        producer.send(topic, value=payload, timestamp_ms=timestamp_ms)
         time.sleep(1)
     except KeyboardInterrupt:
         print("Terminating gracefully...")

2cf0ff5b960fa7c67e07b000b61d735ddaca550b
Simon Mihevc <simon.mihevc@gmail.com>
2019-05-19 20:07:14 +0200

    Added reading of Clj/Streamer processed messages

--- demo.py
+++ demo.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.ERROR)
 
 hosts = ['localhost:9092']
 topic = 'kafka-demo'
+topic_count = 'kafka-demo-count'
 ident = "epic-iot-1"
 
 def kafka_serializer(data):
@@ -19,12 +20,14 @@ def kafka_deserializer(data):
 
 demo_producer = KafkaProducer(bootstrap_servers=hosts, value_serializer=kafka_serializer, request_timeout_ms=1500, max_block_ms=1500)
 demo_consumer = KafkaConsumer(topic, bootstrap_servers=hosts, value_deserializer=kafka_deserializer, request_timeout_ms=1500, consumer_timeout_ms=1500)
+demo_count_consumer = KafkaConsumer(topic_count, bootstrap_servers=hosts, value_deserializer=kafka_deserializer, request_timeout_ms=1500, consumer_timeout_ms=1500)
 
 while True:
     try:
         timestamp_ms = int(round(time.time() * 1000))
         payload = {
             "id": ident,
+            "ts": timestamp_ms,
             "value": 1000 * math.sin(((timestamp_ms / 1000) % 360) / 360.0 * 2 * math.pi)
             }
         print(json.dumps(payload))
@@ -35,6 +38,8 @@ while True:
         if int(timestamp_ms / 1000.0) % 10 == 0:
             for message in demo_consumer:
                 print("Consumer: {ts} {id} {val}".format(ts=message.timestamp, id=message.value['id'], val=message.value['value']))
+            for message in demo_count_consumer:
+                print("Consumer Count: {ts} {id} {val}".format(ts=message.timestamp, id=message.value['id'], val=message.value['value']))
 
     except KeyboardInterrupt:
         print("Terminating gracefully...")

57c58d877f92541eaef14190967ff74eaa697611
Simon Mihevc <simon.mihevc@gmail.com>
2019-05-25 22:26:39 +0200

    Bump dependencies. They are ancient.

--- streamer/project.clj
+++ streamer/project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.apache.kafka/kafka-streams "2.0.0"]
-                 [org.apache.kafka/kafka-clients "2.0.0"]
-                 [org.apache.kafka/kafka-streams-test-utils "2.0.0"]
-                 [org.apache.kafka/kafka-clients "1.1.0" :classifier "test"]
-                 [org.apache.kafka/connect-json "2.0.0"]
-                 [org.springframework.kafka/spring-kafka "1.2.2.RELEASE"]
+                 [org.apache.kafka/kafka-streams "2.2.0"]
+                 [org.apache.kafka/kafka-clients "2.2.0"]
+                 [org.apache.kafka/kafka-streams-test-utils "2.2.0"]
+                 [org.apache.kafka/kafka-clients "2.2.0"]
+                 [org.apache.kafka/connect-json "2.2.0"]
+                 [org.springframework.kafka/spring-kafka "2.2.6.RELEASE"]
                  [org.slf4j/slf4j-log4j12 "1.7.1"]
                  [cheshire "5.8.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
